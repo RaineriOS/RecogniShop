be6a68ca1e77607d09fe000879eda6c5a8657ba2
Daniel Lopez <tauron32@gmail.com>
2013-04-15 16:30:20 +0200

    Nueva interfaz: listado de tiendas

---
+++ RecogniShop/StoresViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoresViewController.h
+//  RecogniShop
+//
+//  Created by Daniel López on 15/04/13.
+//  Copyright (c) 2013 Daniel López. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoresViewController : UIViewController
+
+@end
