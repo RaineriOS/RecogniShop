126c97dfb7f7aa026e6de43bf9f4d6d95383f2f6
Daniel Lopez <tauron32@gmail.com>
2013-04-15 16:25:47 +0200

    Nueva interfaz: Listado de productos

---
+++ RecogniShop/ProductsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductsViewController.h
+//  RecogniShop
+//
+//  Created by Daniel López on 15/04/13.
+//  Copyright (c) 2013 Daniel López. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductsViewController : UIViewController
+
+@end
